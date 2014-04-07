f8f178ef79e9ba281cdefd6b365ddc4834f00ee6
vinhnguyenhoan <vinhnguyenhoan@gmail.com>
2014-04-07 23:36:38 +0700

    set allow access for all client at one resource service

--- src/main/java/com/jinnova/smartpad/resource/AccountResource.java
+++ src/main/java/com/jinnova/smartpad/resource/AccountResource.java
@@ -2,14 +2,14 @@ package com.jinnova.smartpad.resource;
 
 import java.sql.SQLException;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-import com.jinnova.smartpad.partner.IUser;
-import com.jinnova.smartpad.partner.PartnerManager;
 import com.jinnova.smartpad.util.JsonResponse;
 
 @Path("/acc")
@@ -17,10 +17,13 @@ import com.jinnova.smartpad.util.JsonResponse;
 public class AccountResource {
 
 	@GET
-    public JsonResponse getFeed(@QueryParam("userId")String userName, @QueryParam("passwordId")String password) 
+    public JsonResponse getFeed(@QueryParam("userId")String userName, @QueryParam("passwordId")String password, 
+    		@Context HttpServletResponse context) 
     		throws SQLException {
 		//IUser userLogged = PartnerManager.instance.login(userName, password);
 		//User user = new User(userName, password);
+		//context.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+		context.setHeader("Access-Control-Allow-Origin", "*");
 		return new JsonResponse(true, "success");
 	}
 }
